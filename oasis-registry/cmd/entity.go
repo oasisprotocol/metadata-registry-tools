295e7f57463a72c99ba99f69573b6b0347621d20
Tadej Janež <tadej.j@nez.si>
2021-02-10 12:59:28 +0100

    cmd: Add logErrorAndExit() and loadSigner() helper functions
    This reduces length and the number of statements of the doEntityUpdate()
    function.

--- oasis-registry/cmd/entity.go
+++ oasis-registry/cmd/entity.go
@@ -43,6 +43,27 @@ var (
 	entityLogger = logging.GetLogger("cmd/entity")
 )
 
+func logErrorAndExit(msg string, err error) {
+	entityLogger.Error(msg, "err", err)
+	os.Exit(1)
+}
+
+func loadSigner() (signature.Signer, error) {
+	signerDir, err := cmdSigner.CLIDirOrPwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve signer dir: %w", err)
+	}
+	signerFactory, err := cmdSigner.NewFactory(cmdSigner.Backend(), signerDir, signature.SignerEntity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create signer factory: %w", err)
+	}
+	signer, err := signerFactory.Load(signature.SignerEntity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load signer: %w", err)
+	}
+	return signer, nil
+}
+
 func doEntityUpdate(cmd *cobra.Command, args []string) {
 	if len(args) != 1 {
 		entityLogger.Error("expected a single argument")
@@ -54,50 +75,24 @@ func doEntityUpdate(cmd *cobra.Command, args []string) {
 	// Open and parse the passed entity metadata file.
 	rawEntity, err := ioutil.ReadFile(args[0])
 	if err != nil {
-		entityLogger.Error("failed to read entity descriptor",
-			"err", err,
-		)
-		os.Exit(1)
+		logErrorAndExit("failed to read entity descriptor", err)
 	}
 
 	var entity registry.EntityMetadata
 	if err = json.Unmarshal(rawEntity, &entity); err != nil {
-		entityLogger.Error("failed to parse serialized entity metadata",
-			"err", err,
-		)
-		os.Exit(1)
+		logErrorAndExit("failed to parse serialized entity metadata", err)
 	}
 
 	if !viper.GetBool(cfgSkipValidation) {
 		if err = entity.ValidateBasic(); err != nil {
-			entityLogger.Error("provided entity metadata is invalid",
-				"err", err,
-			)
-			os.Exit(1)
+			logErrorAndExit("provided entity metadata is invalid", err)
 		}
 	}
 
 	// Get the signer.
-	signerDir, err := cmdSigner.CLIDirOrPwd()
+	signer, err := loadSigner()
 	if err != nil {
-		entityLogger.Error("failed to retrieve signer dir",
-			"err", err,
-		)
-		os.Exit(1)
-	}
-	signerFactory, err := cmdSigner.NewFactory(cmdSigner.Backend(), signerDir, signature.SignerEntity)
-	if err != nil {
-		entityLogger.Error("failed to create signer factory",
-			"err", err,
-		)
-		os.Exit(1)
-	}
-	signer, err := signerFactory.Load(signature.SignerEntity)
-	if err != nil {
-		entityLogger.Error("failed to load signer",
-			"err", err,
-		)
-		os.Exit(1)
+		logErrorAndExit("failed to load signer", err)
 	}
 
 	// Show descriptor and ask for confirmation.
@@ -120,17 +115,11 @@ func doEntityUpdate(cmd *cobra.Command, args []string) {
 	// Sign the descriptor.
 	signed, err := registry.SignEntityMetadata(signer, &entity)
 	if err != nil {
-		entityLogger.Error("failed to sign metadata",
-			"err", err,
-		)
-		os.Exit(1)
+		logErrorAndExit("failed to sign metadata", err)
 	}
 
 	if err = p.UpdateEntity(signed); err != nil {
-		entityLogger.Error("failed to update metadata",
-			"err", err,
-		)
-		os.Exit(1)
+		logErrorAndExit("failed to update metadata", err)
 	}
 
 	fmt.Printf("Updated entity %s\n", signer.Public())
